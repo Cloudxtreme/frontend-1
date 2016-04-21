bfbbbad2b52ca2b5c8761e54507047a4fc24f39b
Brady Vitrano <bjvitrano@gmail.com>
2016-04-21 15:01:52 -0500

    feat(home): add waiting state for component

--- src/module/common/actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const UPDATE_NAME = 'UPDATE_NAME';
-export const CHANGE_FORM = 'CHANGE_FORM';
--- src/module/common/actions.js
+++
@@ -1,9 +0,0 @@
-import { UPDATE_NAME, CHANGE_FORM } from './actionTypes'
-
-export function updateName(name) {
-    return {type: UPDATE_NAME, name: name}
-}
-
-export function changeForm(form) {
-    return {type: CHANGE_FORM, form: form}
-}
